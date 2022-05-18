1f04be78de53a64778af161a246bff5e8bc8b923
kwamediw <kwamediwanix@gmail.com>
2022-05-18 15:35:22 +0200

    add-signup

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,6 @@
 const userModel = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const login = (req, res, next) => {
   //res.json()
@@ -9,9 +11,19 @@ const signup = async (req, res, next) => {
     const {
       body: { email, password },
     } = req;
-  
-    //Check DB for exisiting user  
-    const user = await userModel.create({ email, password });
+
+    //Check DB for an exisiting user
+    const found = await userModel.findOne({ email });
+    if (found) throw new Error("this user  Exists");
+
+    const hash = await bcrypt.hash(password, 6);
+
+    const user = await userModel.create({ email, password: hash });
+
+    const token = jwt.sign({ id: user._id, email }, process.env.JWT_SECRET, {
+      expiresIn: "180s",
+    });
+
     res.json(user);
   } catch (error) {
     res.status(500).json({ message: error.message });
