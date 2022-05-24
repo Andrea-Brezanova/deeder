fbd7a7bbd8969c00b2a7bd8dc58d374daf950b05
Andrea-Brezanova <andrea.brezan@gmail.com>
2022-05-24 12:02:13 +0200

    add homepage routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,10 @@ import About from "./Components/About";
 import SignIn from "./Components/SignIn";
 import SignUp from "./Components/SignUp";
 import AllPosts from "./Components/AllPosts";
+import OfferHelp from "./Components/OfferHelp";
+import GetHelp from "./Components/GetHelp";
+import OfferCompany from "./Components/OfferCompany";
+import GetCompany from "./Components/GetCompany";
 import NoPage from "./Components/NoPage";
 import { Routes, Route } from "react-router-dom";
 
@@ -20,6 +24,10 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/allposts" element={<AllPosts />} />
         <Route path="*" element={<NoPage />} />
+        <Route path="/offer-help" element={<OfferHelp />} />
+        <Route path="/get-help" element={<GetHelp />} />
+        <Route path="/offer-company" element={<OfferCompany />} />
+        <Route path="/get-company" element={<GetCompany />} />
       </Routes>
     </div>
   );
