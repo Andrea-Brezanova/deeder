8372e4728ee0ff9729012fb426afbb9b6d016ac7
Andrea-Brezanova <andrea.brezan@gmail.com>
2022-05-24 17:36:18 +0200

    removed Navbar from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,23 +13,21 @@ import OfferCompany from "./Components/OfferCompany";
 import GetCompany from "./Components/GetCompany";
 import NoPage from "./Components/NoPage";
 
-
-
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Layout />} >
-        <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/allposts" element={<AllPosts />} />
-        <Route path="/offer-help" element={<OfferHelp />} />
-        <Route path="/get-help" element={<GetHelp />} />
-        <Route path="/offer-company" element={<OfferCompany />} />
-        <Route path="/get-company" element={<GetCompany />} />
-        <Route path="*" element={<NoPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/allposts" element={<AllPosts />} />
+          <Route path="/offer-help" element={<OfferHelp />} />
+          <Route path="/get-help" element={<GetHelp />} />
+          <Route path="/offer-company" element={<OfferCompany />} />
+          <Route path="/get-company" element={<GetCompany />} />
+          <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
     </div>
