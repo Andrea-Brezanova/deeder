a9dd07d43322b08bd31937b7473a45abf2bd3408
Andrea-Brezanova <andrea.brezan@gmail.com>
2022-05-26 14:19:02 +0200

    added

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Navigate, useNavigate } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div>
-        <Link to="/offer-help">Offer Help</Link>
-        <Link to="/get-help">Get Help</Link>
-        <Link to="/offer-company">Offer Company </Link>
-        <Link to="/get-company">Get Company</Link>
+    <div> 
+       <img src="/Images"></img>
     </div>
   )
 }
