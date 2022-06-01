dc8e77bd48468b1c76e9d7216e6ca820e6e42bf3
kwamediw <kwamediwanix@gmail.com>
2022-06-01 16:59:51 +0200

    update-styles

--- frontend/src/Components/GetCompany.js
+++ frontend/src/Components/GetCompany.js
@@ -7,6 +7,7 @@ export default function GetHelp() {
   const [subcategories, setSubcategories] = useState([]);
   const [categories, setcategories] = useState([]);
 
+  console.log("HERE", subcategories);
   const {
     register,
     handleSubmit,
@@ -40,44 +41,55 @@ export default function GetHelp() {
         console.log(err);
       });
   }, []);
+  const names = subcategories.map((x) => x.name);
+  console.log("NAME", names);
 
   return (
     <>
-      <div className="write-request-form">
-        Please fill out the form below. <br />
-        Remember to say what matters!
-      </div>
-      <div className="offer">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <textarea
-            placeholder="Write something about yourself and get in touch!"
-            {...register("body", { required: true })}
-          ></textarea>
-
-          {errors.body && <div className="alert">Text is required</div>}
-          <button className="get-btn">Submit</button>
-        </form>
-        <div>
-          <div className="select-subcategory">
-            <p>Please select a subcategory</p>
-            <select {...register("subcategory", { required: true })}>
+      <div className="request-body">
+        <div className="request-form">
+          <p className="request-info">
+            Please fill out the form below. Remember to say what matters!
+          </p>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <textarea
+              placeholder="Write something about yourself and get in touch!"
+              {...register("body", { required: true })}
+            ></textarea>
+            {errors.body && (
+              <div className="alert">This field can not be empty</div>
+            )}
+            <button className="get-btn">Submit</button>
+          </form>
+        </div>
+        <div className="subcategory-form">
+          <div className="category-list">
+            <p className="request-info">Please select a subcategory</p>
+            <select
+              style="min-height:40px;"
+              {...register("subcategory", { required: true })}
+            >
               {subcategories.map((subcategory) => {
                 return (
-                  <option value={subcategory._id}>{subcategory.name}</option>
+                  <option
+                    className="option1"
+                    value={subcategory._id}
+                    length="10"
+                  >
+                    {subcategory.name}
+                  </option>
                 );
               })}
             </select>
           </div>
-          <div className="select-category">
-            <p>Please select a category</p>
-            <select {...register("category", { required: true })}>
-              {categories.map((category) => {
-                return (
-                  <option value={category._id}>{category.name}</option>
-                );
-              })}
-            </select>
-          </div>
+        </div>
+        <div className="category-form">
+          <p className="request-info">Please select a category</p>
+          <select {...register("category", { required: true })}>
+            {categories.map((category) => {
+              return <option value={category._id}>{category.name}</option>;
+            })}
+          </select>
         </div>
       </div>
     </>
--- frontend/src/Components/GetHelp.js
+++ frontend/src/Components/GetHelp.js
@@ -5,6 +5,7 @@ import axios from "axios";
 export default function GetHelp() {
   const [body, setBody] = useState("");
   const [subcategories, setSubcategories] = useState([]);
+  const [categories, setcategories] = useState([]);
 
   const {
     register,
@@ -31,6 +32,7 @@ export default function GetHelp() {
   useEffect(() => {
     axios
       .get("http://localhost:3001/subcategories")
+      // .get("http://localhost:3001/categories")
       .then((res) => {
         setSubcategories(res.data);
       })
@@ -41,26 +43,42 @@ export default function GetHelp() {
 
   return (
     <>
-      <div className="write-request-form">PULL USER DATA FROM DB</div>
-      <div className="offer">
-        
-          <div className="searchInputs">
+      <div className="request-body">
+        <div className="request-form">
+          <p className="request-info">
+            Please fill out the form below. Remember to say what matters!
+          </p>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <textarea
+              placeholder="Write something about yourself and get in touch!"
+              {...register("body", { required: true })}
+            ></textarea>
+            {errors.body && (
+              <div className="alert">This field can not be empty</div>
+            )}
+            <button className="get-btn">Submit</button>
+          </form>
+        </div>
+        <div className="subcategory-form">
+          <div className="category-list">
+            <p className="request-info">Please select a subcategory</p>
             <select {...register("subcategory", { required: true })}>
               {subcategories.map((subcategory) => {
-                return <option value={subcategory._id}>{subcategory.name}</option>;
+                return (
+                  <option value={subcategory._id}>{subcategory.name}</option>
+                );
               })}
             </select>
           </div>
-        
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <textarea
-            placeholder="Write something about yourself and get in touch!"
-            {...register("body", { required: true })}
-          ></textarea>
-
-          {errors.body && <div className="alert">Text is required</div>}
-          <button className="get-btn">Submit</button>
-        </form>
+        </div>
+        <div className="category-form">
+          <p className="request-info">Please select a category</p>
+          <select {...register("category", { required: true })}>
+            {categories.map((category) => {
+              return <option value={category._id}>{category.name}</option>;
+            })}
+          </select>
+        </div>
       </div>
     </>
   );
