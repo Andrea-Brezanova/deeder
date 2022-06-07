984762e6c128bb8e462d3ef2cc868d352cdc23ae
Andrea-Brezanova <andrea.brezan@gmail.com>
2022-06-07 21:48:30 +0200

    thank you message changed

--- frontend/src/Components/ThankYou.js
+++ frontend/src/Components/ThankYou.js
@@ -5,8 +5,10 @@ function ThankYou() {
 
   return (
     <>
-      <span className="animate__animated animate__bounce"><img className="heart-logo" src="../Images/heart.gif"></img></span><span className="thanks-entry">Thanks, Your Entry has been Submitted</span>
-      
+      <span className="animate__animated animate__bounce">
+        <img className="heart-logo" src="../Images/heart.gif"></img></span>
+        <span className="thanks-entry">We've got your message! </span>
+
     </>
   );
 }
